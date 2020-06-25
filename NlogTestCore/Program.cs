2636953ae7f126ec41c90f5e785fbc69d18104c9
MBS <shankar.maruvada@gmail.com>
2020-06-25 11:42:23 -0500

    Add project files.

---
+++ NlogTestCore/Program.cs
@@ -0,0 +1,27 @@
+﻿using NLog;
+using System;
+
+namespace NlogTestCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            Logger logger = LogManager.GetLogger("databaseLogger");
+            try
+            {               
+                int zero = 0;
+                int result = 1 / zero;
+                logger.Info("hi");
+
+            }
+
+            catch(DivideByZeroException ex)
+            {
+                
+                logger.Error(ex, "whoops");
+            }
+        }
+    }
+}
